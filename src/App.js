f2e1f151e275f23d4173d4a23e3ebd0350957a15
AKBAR SALEEM SHAIK <akbarsaleemshaik@AKBARs-MacBook-Air.local>
2022-02-28 15:39:20 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>రాధేశ్యామ్‌</h1>
+      <img src='https://www.deccanherald.com/sites/dh/files/articleimages/2020/07/10/RS-1594356518.jpg' width='300' height='450'>
+      </img>
+    
+      <h2>Nagumomu Thaarale Song 🎶</h2>
+      <audio src='./assets/audio/Nagumomu Thaarale.mp3' type='audio/mp3' controls></audio>
+      <h2>Sanchari Song 🎶</h2>
+      <audio src='./assets/audio/Sanchari.mp3' type='audio/mp3' controls></audio>
+      <h2>Ee Raathale Song 🎶</h2>
+      <audio src='./assets/audio/Ee Raathale.mp3' type='audio/mp3' controls></audio>
+      <h2>Radhe Shyam Telugu Glimpse | Prabhas | Pooja Hegde</h2>
+      <video src='./assets/audio/RS.mp4' type='video/mp4'width='500' height='300' autoPlay muted controls></video>
+    
+    </div>
+  );
+}
+export default App;
